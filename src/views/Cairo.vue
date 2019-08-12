41d924caa749ee6eab28ea5aa72ca70fc1d8600c
pierrealixt <pierrealix@icloud.com>
2019-08-12 16:59:10 +0200

    new background

---
+++ src/views/Cairo.vue
@@ -0,0 +1,13 @@
+<template>
+    <b>hello</b>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+
+@Component({
+  components: {
+  },
+})
+export default class Cairo extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="home">
+  <div>
+    hello
+  </div>
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  </div> -->
 </template>
 
 <script lang="ts">
@@ -16,3 +19,13 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss">
+html {
+  background: url('../assets/HeaderImage.png') no-repeat center center fixed;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+}
+</style>
