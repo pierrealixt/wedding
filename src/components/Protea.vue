bede240aaefef99af0ba7437296bffce13354497
Pierre-Alix Tremblay <pierrealix@kartoza.com>
2019-08-18 18:30:52 +0200

    responsive design

---
+++ src/components/Protea.vue
@@ -0,0 +1,66 @@
+<template>
+    <div :class="getClass()">
+        <img  src='../assets/protea.svg' alt='protea' />
+    </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Protea extends Vue {
+    @Prop() private position!: string;
+
+    public getClass(): string  {
+        return 'protea protea-' + this.position;
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+
+// @media (min-width: 1025px) and (max-width: 1280px) {
+//     div.protea {
+//         width: 15vw;
+//     }
+
+// }
+
+div.protea {
+    position: absolute;
+    bottom: 0;
+    img {
+        width: 192px;
+        height: 300px;
+    }
+}
+
+div.protea-left {
+    left: 0;
+}
+
+div.protea-right {
+    right: 0;
+    img {
+        transform: scaleX(-1);
+    }
+}
+
+@media (min-width: 320px) and (max-width: 414px) {
+    div.protea {
+        img {
+            width: 60px;
+            height: 120px;
+        }
+    }
+}
+
+@media (min-width: 568px) and (max-width: 812px) {
+    div.protea {
+        img {
+            width: 50px;
+            height: 100px;
+        }
+    }
+}
+</style>
--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div :class="getCSSClasses()">
         <p v-bind:style="{fontSize: fontSize + 'px' }">
             {{ text.line1 }}
             <br />
@@ -15,13 +15,18 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class MyText extends Vue {
     @Prop() private text!: string;
     @Prop() private fontSize!: string;
+    @Prop() private css!: string;
+    public getCSSClasses(): string  {
+        return this.css;
+    }
+
 }
 </script>
 
 <style scoped lang="scss">
 p {
-    font-family: Helvetica Neue,Helvetica,Arial,sans-serif; 
-    color: #505B29;
+    font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+    color: #FFFFFF;
     text-align: center;
     text-transform: uppercase;
     letter-spacing: 2.6px;
